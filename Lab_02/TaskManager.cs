633732e76f5296b94bde8e28d872874ba454aa51
cubano123 <106766351+cubano123@users.noreply.github.com>
2022-06-10 10:29:53 +0200

    Add files via upload

---
+++ Lab_02/TaskManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp20
+{
+    class TaskManager
+    {
+        private List<ITask> waitingTasks;
+        private List<ITask> finishedTasks;
+
+        public void AddTask(ITask task)
+        {
+            waitingTasks.Add(task);
+        }
+    }
+}
